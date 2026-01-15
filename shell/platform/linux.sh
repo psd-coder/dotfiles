b48ab54c2011d296c7cfa1ffa7a7082820ee1e67
Pavel Grinchenko <psd-coder@evilmartians.com>
2026-01-15 16:24:58 +0000

    Add shell/platform/linux.sh for Linux-specific exports
    Contains 1Password SSH agent path, LS_COLORS, pnpm home,
    and fnm path for Linux.

---
+++ shell/platform/linux.sh
@@ -0,0 +1,18 @@
+# Linux-specific exports
+
+# 1Password SSH Agent
+if [[ -S "$HOME/.1password/agent.sock" ]]; then
+  export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
+fi
+
+# Linux colors (LS_COLORS instead of LSCOLORS)
+export LS_COLORS='di=1;34:ln=1;36:so=1;31:pi=1;33:ex=1;32:bd=1;34;46:cd=1;34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43'
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+path_prepend "$PNPM_HOME"
+
+# fnm (Fast Node Manager) - installed via script on Linux
+if [[ -d "$HOME/.local/share/fnm" ]]; then
+  path_prepend "$HOME/.local/share/fnm"
+fi
